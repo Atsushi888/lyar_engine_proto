982ed8e4d5a856e4e9fccd49161925bf79692fe8
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-11-09 02:55:31 +0900

    Update __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -5,4 +5,4 @@ from .debug_panel import DebugPanel
 from .chat_log import ChatLog
 from .player_input import PlayerInput
 
-# __all__ = ["PreflightChecker", "DebugPanel", "ChatLog", "PlayerInput" ]
+__all__ = ["PreflightChecker", "DebugPanel", "ChatLog", "PlayerInput" ]
