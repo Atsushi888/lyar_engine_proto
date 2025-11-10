9c1577adee5fccd8e86bb420867c0733f3e13870
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2025-11-11 05:47:55 +0900

    Clear components/__init__.py file
    Remove all content from __init__.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,9 +1,1 @@
-# components/__init__.py
 
-from .preflight import PreflightChecker
-from .debug_panel import DebugPanel
-from .chat_log import ChatLog
-from .player_input import PlayerInput
-from .model_viewer import MultiModelViewer
-
-__all__ = ["PreflightChecker", "DebugPanel", "ChatLog", "PlayerInput", "MultiModelViewer" ]
